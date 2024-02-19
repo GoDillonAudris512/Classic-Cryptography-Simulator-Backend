9f4bc556031d38cfae7bef8609c3745d2e440f24
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2024-02-19 13:53:20 +0700

    chore: enable cors for all endpoints

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/joho/godotenv"
+	"github.com/rs/cors"
 	"log"
 	"net/http"
 	"os"
@@ -17,10 +18,19 @@ func main() {
 
 	router := router.Router()
 
+	corsOptions := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"POST"},
+		AllowedHeaders:   []string{"Content-Type"},
+		AllowCredentials: true,
+	})
+
+	corsHandler := corsOptions.Handler(router)
+
 	var port = EnvPortOr("8080")
 	fmt.Println("Starting server...")
 	fmt.Println("Listening from http://localhost" + port + "/api")
-	log.Fatal(http.ListenAndServe(port, router))
+	log.Fatal(http.ListenAndServe(port, corsHandler))
 }
 
 func EnvPortOr(port string) string {
--- middleware/generalHandlers.go
+++ middleware/generalHandlers.go
@@ -6,12 +6,6 @@ import (
 )
 
 func MainHandler(w http.ResponseWriter, r *http.Request) {
-	// Allow all origin to handle cors issue
-	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
 	fmt.Fprintf(w, "Hello, welcome to Classic Cryptography Simulator Web Service!")
 }
 
